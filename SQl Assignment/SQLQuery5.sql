99037b702dec130e93d6a10a4f07cb00bd34d904
prabhavgoel <prabhav.goel@nagarro.com>
2023-03-27 14:49:39 +0530

    Upload New File

---
+++ SQl Assignment/SQLQuery5.sql
@@ -0,0 +1,8 @@
+GO
+CREATE PROCEDURE Person.up_getNamesByType
+	@Type nchar(2) = 'EM'
+AS
+SELECT FirstName FROM Person.Person WHERE PersonType = @Type
+GO
+
+EXEC Person.up_getNamesByType @Type ='SC'
