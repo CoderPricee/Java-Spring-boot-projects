2416b2bc850243f1df114f083a20930b66980b60
prabhavgoel <prabhav.goel@nagarro.com>
2023-03-27 14:49:25 +0530

    Upload New File

---
+++ SQl Assignment/SQLQuery4.sql
@@ -0,0 +1,19 @@
+
+CREATE FUNCTION Sales.uf_getProducts(@SalesOrderID int, @CurrencyCode nchar(3) ,@CurrencyRateDate datetime)
+	RETURNS TABLE 
+AS
+	RETURN
+	WITH Products
+	AS (
+		SELECT *
+		FROM Sales.SalesOrderDetail AS sod 
+		WHERE sod.SalesOrderID = @SalesOrderID
+	)
+	SELECT p.ProductID, p.OrderQty, p.UnitPrice, p.UnitPrice*scr.EndOfDayRate AS 'Converted Price'
+	FROM Products AS p, Sales.CurrencyRate AS scr
+	WHERE scr.ToCurrencyCode = @CurrencyCode
+		AND scr.CurrencyRateDate = @CurrencyRateDate
+GO
+
+SELECT *FROM Sales.uf_getProducts(43660,'CNY','2005-07-01 00:00:00.000'); 
+
