752ee85261bd9c1645189756f96ef86b7d5312ef
prabhavgoel <prabhav.goel@nagarro.com>
2023-03-28 13:18:38 +0530

    Delete SQLQuery5.sql

--- SQl Assignment/SQLQuery5.sql
+++
@@ -1,8 +0,0 @@
-GO
-CREATE PROCEDURE Person.up_getNamesByTypeFirstName
-	@Type nchar(2) = 'EM'
-AS
-SELECT FirstName FROM Person.Person WHERE PersonType = @Type
-GO
-
-EXEC Person.up_getNamesByTypeFirstName @Type ='SC'
